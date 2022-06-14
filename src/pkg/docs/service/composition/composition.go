215eb645f30510fa3c7966d5f406ca93400c0279
Stepan Prishepenko <itman116@gmail.com>
2022-06-14 12:02:40 +0300

    #IPEHR-45 add in composition create with id feature

--- src/pkg/docs/service/composition/composition.go
+++ src/pkg/docs/service/composition/composition.go
@@ -4,10 +4,13 @@ import (
 	"encoding/json"
 	"github.com/google/uuid"
 	"hms/gateway/pkg/config"
-	"log"
-
+	"hms/gateway/pkg/crypto/chacha_poly"
 	"hms/gateway/pkg/docs/model"
+	"hms/gateway/pkg/docs/model/base"
 	"hms/gateway/pkg/docs/service"
+	"hms/gateway/pkg/docs/types"
+	"log"
+	"time"
 )
 
 type CompositionService struct {
@@ -39,108 +42,85 @@ func (s CompositionService) Create(userId string, request *model.CompositionCrea
 	return s.CreateWithId(userId, uuid.New().String(), request) // TODO no new???
 }
 
-func (s CompositionService) CreateWithId(userId, ehrId string, request *model.CompositionCreateRequest) (*model.Composition, error) {
-	var composition model.Composition
+func (s CompositionService) CreateWithId(userId, ehrId string, request *model.CompositionCreateRequest) (composition *model.Composition, err error) {
+	composition = &model.Composition{}
 
-	//composition.SystemId.Value = s.Doc.GetSystemId() //TODO
-	//composition.EhrId.Value = ehrId
-	//
-	//composition.EhrStatus.Id.Type = "OBJECT_VERSION_ID"
-	//composition.EhrStatus.Id.Value = uuid.New().String() + "::openEHRSys.example.com::1"
-	//composition.EhrStatus.Namespace = "local"
-	//composition.EhrStatus.Type = "EHR_STATUS"
-	//
-	//composition.EhrAccess.Id.Type = "OBJECT_VERSION_ID"
-	//composition.EhrAccess.Id.Value = uuid.New().String() + "::openEHRSys.example.com::1"
-	//composition.EhrAccess.Namespace = "local"
-	//composition.EhrAccess.Type = "EHR_ACCESS"
-	//
-	//composition.TimeCreated.Value = time.Now().Format("2006-01-02T15:04:05.999-07:00")
+	composition.Type = types.COMPOSITION.String()
+	// TODO cant comprehend should we use data from request (because validation) or create new one?
+	composition.ArchetypeNodeId = request.ArchetypeNodeID
+	composition.Name.Value = request.Name.Value
+	composition.Uid = &base.ObjectId{
+		Type:  request.UID.Type,
+		Value: request.UID.Value,
+	}
+	composition.ArchetypeDetails = &base.Archetyped{
+		ArchetypeId: base.ObjectId{
+			Value: request.ArchetypeDetails.ArchetypeID.Value,
+		},
+		TemplateId: &base.ObjectId{Value: request.ArchetypeDetails.TemplateID.Value},
+		RmVersion:  request.ArchetypeDetails.RmVersion,
+	}
 
-	// Creating EHR_STATUS base
-	//ehrStatusService := ehr_status.NewEhrStatusService(s.Doc)
-	//
-	//ehrStatusId := ehr.EhrStatus.Id.Value
-	//subjectId := request.Subject.ExternalRef.Id.Value
-	//subjectNamespace := request.Subject.ExternalRef.Namespace
-	//
-	//ehrStatusDoc := ehrStatusService.Create(ehrStatusId, subjectId, subjectNamespace)
+	// TODO fill others
+	//composition.Language
+	//composition.Territory
+	//composition.Category
+	//composition.Composer
+	//composition.Context
+	//composition.Content
 
-	err := s.save(userId, &composition)
+	err = s.save(userId, ehrId, composition)
 
-	return &composition, err
+	return composition, err
 }
 
-func (s CompositionService) save(userId string, doc *model.Composition) error {
-	//docBytes, err := s.MarshalJson(doc)
-	_, err := s.MarshalJson(doc)
+func (s CompositionService) save(userId string, ehrId string, doc *model.Composition) error {
+	docBytes, err := s.MarshalJson(doc)
 	if err != nil {
 		log.Println(err)
 		return err
 	}
 
 	// Document encryption key generationg
-	//key := chacha_poly.GenerateKey()
-	//
-	//// Document encryption
-	//docEncrypted, err := key.EncryptWithAuthData(docBytes, []byte(doc.EhrId.Value))
-	//if err != nil {
-	//	log.Println(err)
-	//	return err
-	//}
-	//
-	//// Storage saving
-	//docStorageId, err := s.Doc.Storage.Add(docEncrypted)
-	//if err != nil {
-	//	log.Println(err)
-	//	return err
-	//}
-	//
-	//// Index EHR userId -> docStorageId
-	//if err = s.Doc.EhrsIndex.Add(userId, docStorageId); err != nil {
-	//	log.Println(err)
-	//	return err
-	//}
-	//
-	//// Index Docs ehr_id -> doc_meta
-	//docIndex := &model.DocumentMeta{
-	//	TypeCode:  types.EHR,
-	//	StorageId: docStorageId,
-	//	Timestamp: uint32(time.Now().Unix()),
-	//}
-	//// First record in doc index
-	//if err = s.Doc.DocsIndex.Add(doc.EhrId.Value, docIndex); err != nil {
-	//	log.Println(err)
-	//	return err
-	//}
-	//
-	//// Index Access
-	//if err = s.Doc.AccessIndex.Add(userId, docStorageId, key.Bytes()); err != nil {
-	//	log.Println(err)
-	//	return err
-	//}
+	key := chacha_poly.GenerateKey()
 
-	// Saving EHR status
-	//ehrStatusService := ehr_status.NewEhrStatusService(s.Doc)
-	//if err = ehrStatusService.Save(doc.EhrId.Value, userId, ehrStatusDoc); err != nil {
-	//	log.Println(err)
-	//	return err
-	//}
+	// Document encryption
+	docEncrypted, err := key.EncryptWithAuthData(docBytes, []byte(ehrId))
+	if err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// Storage saving
+	docStorageId, err := s.Doc.Storage.Add(docEncrypted)
+	if err != nil {
+		log.Println(err)
+		return err
+	}
 	//
-	//// Subject index
-	//err = s.addSubjectIndex(doc.EhrId.Value, ehrStatusDoc)
-	//if err != nil {
-	//	log.Println(err)
-	//	return err
-	//}
+	// Index EHR userId -> docStorageId
+	if err = s.Doc.EhrsIndex.Add(userId, docStorageId); err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// Index Docs ehr_id -> doc_meta
+	docIndex := &model.DocumentMeta{
+		TypeCode:  types.COMPOSITION,
+		StorageId: docStorageId,
+		Timestamp: uint64(time.Now().UnixNano()),
+	}
+	// First record in doc index
+	if err = s.Doc.DocsIndex.Add(ehrId, docIndex); err != nil {
+		log.Println(err)
+		return err
+	}
+
+	// Index Access
+	if err = s.Doc.DocAccessIndex.Add(userId, docStorageId, key.Bytes()); err != nil {
+		log.Println(err)
+		return err
+	}
 
 	return nil
 }
-
-// Add EHR status subject index
-//func (s CompositionService) addSubjectIndex(ehrId string, ehrStatusDoc *model.EhrStatus) (err error) {
-//	subjectId := ehrStatusDoc.Subject.ExternalRef.Id.Value
-//	subjectNamespace := ehrStatusDoc.Subject.ExternalRef.Namespace
-//	err = s.Doc.SubjectIndex.AddEhrSubjectsIndex(ehrId, subjectId, subjectNamespace)
-//	return
-//}
