d309371472111ab340c8b582f197c345bf6062b2
DS <kube@ya.ru>
2022-07-27 18:02:38 +0500

    ipfsClient.Get

--- src/pkg/storage/ipfs/ipfsClient.go
+++ src/pkg/storage/ipfs/ipfsClient.go
@@ -48,7 +48,7 @@ func (i *Client) Add(fileContent []byte) (string, error) {
 
 	req, err := http.NewRequest(http.MethodPost, url, &requestBody)
 	if err != nil {
-		return "", fmt.Errorf("http.NewRequest error: %w", err)
+		return "", fmt.Errorf("http.NewRequest add error: %w", err)
 	}
 
 	req.Header.Add("Content-Type", multiPartWriter.FormDataContentType())
@@ -72,4 +72,22 @@ func (i *Client) Add(fileContent []byte) (string, error) {
 	resp.Body.Close()
 
 	return result.Hash, nil
+}
+
+// Get file from IPFS node by CID
+// Returns ReadCloser or error
+// Need to close()
+func (i *Client) Get(fileCID string) (io.ReadCloser, error) {
+	url := i.apiURL + "/cat?arg=" + fileCID + "&archive=true"
+
+	resp, err := i.httpClient.Post(url, "", nil)
+	if err != nil {
+		return nil, fmt.Errorf("IPFS get request error: %w", err)
+	}
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("IPFS get request error: %w status %s", errors.ErrCustom, resp.Status)
+	}
+
+	return resp.Body, nil
 }
--- src/pkg/storage/ipfs/ipfsClient_test.go
+++ src/pkg/storage/ipfs/ipfsClient_test.go
@@ -1,9 +1,11 @@
 package ipfs_test
 
 import (
+	"io/ioutil"
+	"testing"
+
 	"hms/gateway/pkg/config"
 	"hms/gateway/pkg/storage/ipfs"
-	"testing"
 )
 
 func TestAddFile(t *testing.T) {
@@ -12,7 +14,7 @@ func TestAddFile(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	expectedCid := "QmPYKPZhu6LdLrZJUbmUTPFCogwmmenaKMH5XMsrEBNG3m"
+	//expectedCid := "QmPYKPZhu6LdLrZJUbmUTPFCogwmmenaKMH5XMsrEBNG3m"
 	fileContent := []byte("dfgg dtghreyh .sm,dfdsoiqwuefbw3586 (!!!) test one")
 
 	testIpfsClient := ipfs.NewClient(cfg.Storage.Ipfs.EndpointURL)
@@ -22,7 +24,22 @@ func TestAddFile(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	if cid != expectedCid {
-		t.Fatalf("Received wrong CID (expected, received): %s, %s", expectedCid, cid)
+	fileCloser, err := testIpfsClient.Get(cid)
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer fileCloser.Close()
+
+	fileContent2, err := ioutil.ReadAll(fileCloser)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if len(fileContent) != len(fileContent2) {
+		t.Fatalf("Received file length is wrong (expected, received): %d, %d", len(fileContent), len(fileContent2))
+	}
+
+	if string(fileContent) != string(fileContent2) {
+		t.Fatalf("Received wrong file (expected, received): %s, %s", fileContent, fileContent2)
 	}
 }
