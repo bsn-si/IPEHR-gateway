00039ec5f8a8d3a52d0bce89987d047ba1ff9e9f
DS <kube@ya.ru>
2022-11-09 14:30:03 +0500

    fix linters

--- src/pkg/api/auth.go
+++ src/pkg/api/auth.go
@@ -11,14 +11,16 @@ import (
 )
 
 func auth(a *API, exceptions ...string) func(*gin.Context) {
+	reCheckRegister := regexp.MustCompile("^[0-9a-f]{12}_register$")
+
 	return func(c *gin.Context) {
 		// Exception strings
 		for _, ex := range exceptions {
 			switch ex {
 			case "userRegister":
 				reqID := c.Param("reqID")
-				match, _ := regexp.MatchString("^[0-9a-f]{12}_register$", reqID)
-				if match {
+
+				if reCheckRegister.MatchString(reqID) {
 					userID := c.Request.Header.Get("AuthUserId")
 					c.Set("userID", userID)
 					c.Next()
