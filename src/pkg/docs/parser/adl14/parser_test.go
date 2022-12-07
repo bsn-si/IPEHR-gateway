d4eb168f5051de4579cb2688b4e66fe3d93965ce
Stepan Prishepenko <itman116@gmail.com>
2022-12-07 11:06:36 +0300

    IPEHR-167: change parser tests after review

--- src/pkg/docs/parser/adl14/parser_test.go
+++ src/pkg/docs/parser/adl14/parser_test.go
@@ -6,23 +6,28 @@ import (
 )
 
 func TestParser_Validate(t *testing.T) {
-	p := NewParser()
-
-	t.Run("Invalid XML", func(t *testing.T) {
-		r := p.Validate([]byte(`<xml>invalid>/`), model.ADLTypeXML)
-
-		if r {
-			t.Errorf("adl14.Validate() xml is valid")
-			return
-		}
-	})
-
-	t.Run("Valid XML", func(t *testing.T) {
-		r := p.Validate([]byte(`<xml></xml>`), model.ADLTypeXML)
-
-		if !r {
-			t.Errorf("adl14.Validate() xml is invalid")
-			return
-		}
-	})
+	tests := []struct {
+		name   string
+		xmlStr string
+		want   bool
+	}{
+		{
+			"1. Invalid XML",
+			`<xml>invalid/`,
+			false,
+		},
+		{
+			"2. Valid XML",
+			`<xml>val</xml>`,
+			true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			parser := NewParser()
+			if parser.Validate([]byte(tt.xmlStr), model.ADLTypeXML) != tt.want {
+				t.Error("adl14.Validate() error")
+			}
+		})
+	}
 }
