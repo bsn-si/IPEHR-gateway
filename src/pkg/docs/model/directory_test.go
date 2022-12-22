19f947f964931f2263448a2f7890dd21ff760be0
Stepan Prishepenko <itman116@gmail.com>
2022-12-22 09:23:17 +0300

    IPEHR-141 add test with subfolders into DIRECTORY model

--- src/pkg/docs/model/directory_test.go
+++ src/pkg/docs/model/directory_test.go
@@ -94,51 +94,191 @@ func TestDirectory_UnmarshalJSON(t *testing.T) {
 			},
 			false,
 		},
-		//{
-		//	"4. empty directory with details",
-		//	directoryTestData{
-		//		d: model.Directory{
-		//			Locatable: base.Locatable{Type: base.FolderItemType, Name: base.NewDvText("root"),
-		//				ArchetypeNodeID: "openEHR-EHR-FOLDER.generic.v1"},
-		//			FeederAudit: base.FeederAudit{},
-		//			Folders:     nil,
-		//			Details:     base.ItemStructure{},
-		//			Items: []model.DirectoryItem{
-		//				{
-		//					ID: base.UIDBasedID{
-		//						ObjectID: base.ObjectID{
-		//							Type:  base.HierObjectIDItemType,
-		//							Value: "replaceme",
-		//						},
-		//					},
-		//					Type:      base.VersionCompositionItemType,
-		//					Namespace: "my.system.id",
-		//				},
-		//			},
-		//		},
-		//		JSON: []byte(`
-		//			{
-		//			  "_type": "FOLDER",
-		//			  "name": {
-		//				"_type": "DV_TEXT",
-		//				"value": "root"
-		//			  },
-		//			  "archetype_node_id": "openEHR-EHR-FOLDER.generic.v1",
-		//			  "items": [
-		//				{
-		//					"id": {
-		//						"_type": "HIER_OBJECT_ID",
-		//						"value": "replaceme"
-		//					},
-		//					"namespace": "my.system.id",
-		//					"type": "VERSIONED_COMPOSITION"
-		//				}
-		//			  ]
-		//			}
-		//		`),
-		//	},
-		//	false,
-		//},
+		{
+			"4. directory with subfolders",
+			directoryTestData{
+				d: model.Directory{
+					Locatable: base.Locatable{
+						Type:            base.FolderItemType,
+						Name:            base.NewDvText("root"),
+						ArchetypeNodeID: "openEHR-EHR-FOLDER.generic.v1",
+					},
+					FeederAudit: base.FeederAudit{},
+					Details:     base.ItemStructure{},
+					Folders: []model.Directory{
+						{
+							Locatable: base.Locatable{
+								Type:            base.FolderItemType,
+								Name:            base.NewDvText("emergency"),
+								ArchetypeNodeID: "openEHR-EHR-FOLDER.generic.v1",
+							},
+							FeederAudit: base.FeederAudit{},
+							Details:     base.ItemStructure{},
+							Folders: []model.Directory{
+								{
+									Locatable: base.Locatable{
+										Type:            base.FolderItemType,
+										Name:            base.NewDvText("episode_x"),
+										ArchetypeNodeID: "openEHR-EHR-FOLDER.generic.v1",
+									},
+									FeederAudit: base.FeederAudit{},
+									Details:     base.ItemStructure{},
+									Folders: []model.Directory{
+										{
+											Locatable: base.Locatable{
+												Type:            base.FolderItemType,
+												Name:            base.NewDvText("summary_compo_x"),
+												ArchetypeNodeID: "openEHR-EHR-FOLDER.generic.v1",
+											},
+											FeederAudit: base.FeederAudit{},
+											Details:     base.ItemStructure{},
+											Folders:     nil,
+										},
+									},
+								},
+								{
+									Locatable: base.Locatable{
+										Type:            base.FolderItemType,
+										Name:            base.NewDvText("episode_y"),
+										ArchetypeNodeID: "openEHR-EHR-FOLDER.generic.v1",
+									},
+									FeederAudit: base.FeederAudit{},
+									Details:     base.ItemStructure{},
+									Folders: []model.Directory{
+										{
+											Locatable: base.Locatable{
+												Type:            base.FolderItemType,
+												Name:            base.NewDvText("summary_compo_y"),
+												ArchetypeNodeID: "openEHR-EHR-FOLDER.generic.v1",
+											},
+											FeederAudit: base.FeederAudit{},
+											Details:     base.ItemStructure{},
+											Folders:     nil,
+										},
+									},
+								},
+							},
+						},
+						{
+							Locatable: base.Locatable{
+								Type:            base.FolderItemType,
+								Name:            base.NewDvText("hospitalization"),
+								ArchetypeNodeID: "openEHR-EHR-FOLDER.generic.v1",
+							},
+							FeederAudit: base.FeederAudit{},
+							Details:     base.ItemStructure{},
+							Folders: []model.Directory{
+								{
+									Locatable: base.Locatable{
+										Type:            base.FolderItemType,
+										Name:            base.NewDvText("summary_compo_z"),
+										ArchetypeNodeID: "openEHR-EHR-FOLDER.generic.v1",
+									},
+									FeederAudit: base.FeederAudit{},
+									Details:     base.ItemStructure{},
+									Folders:     nil,
+								},
+							},
+						},
+						{
+							Locatable: base.Locatable{
+								Type:            base.FolderItemType,
+								Name:            base.NewDvText("foldername-w-special-chars"),
+								ArchetypeNodeID: "openEHR-EHR-FOLDER.generic.v1",
+							},
+							FeederAudit: base.FeederAudit{},
+							Details:     base.ItemStructure{},
+							Folders:     nil,
+						},
+					},
+				},
+				JSON: []byte(`
+					{
+					  "_type": "FOLDER",
+					  "name": {
+						"_type": "DV_TEXT",
+						"value": "root"
+					  },
+					  "archetype_node_id": "openEHR-EHR-FOLDER.generic.v1",
+					  "folders": [
+						{
+						  "_type": "FOLDER",
+						  "name": {
+							"_type": "DV_TEXT",
+							"value": "emergency"
+						  },
+						  "archetype_node_id": "openEHR-EHR-FOLDER.generic.v1",
+						  "folders": [
+							{
+							  "_type": "FOLDER",
+							  "name": {
+								"_type": "DV_TEXT",
+								"value": "episode_x"
+							  },
+							  "archetype_node_id": "openEHR-EHR-FOLDER.generic.v1",
+							  "folders": [
+								{
+								  "_type": "FOLDER",
+								  "name": {
+									"_type": "DV_TEXT",
+									"value": "summary_compo_x"
+								  },
+								  "archetype_node_id": "openEHR-EHR-FOLDER.generic.v1"
+								}
+							  ]
+							},
+							{
+							  "_type": "FOLDER",
+							  "name": {
+								"_type": "DV_TEXT",
+								"value": "episode_y"
+							  },
+							  "archetype_node_id": "openEHR-EHR-FOLDER.generic.v1",
+							  "folders": [
+								{
+								  "_type": "FOLDER",
+								  "name": {
+									"_type": "DV_TEXT",
+									"value": "summary_compo_y"
+								  },
+								  "archetype_node_id": "openEHR-EHR-FOLDER.generic.v1"
+								}
+							  ]
+							}
+						  ]
+						},
+						{
+						  "_type": "FOLDER",
+						  "name": {
+							"_type": "DV_TEXT",
+							"value": "hospitalization"
+						  },
+						  "archetype_node_id": "openEHR-EHR-FOLDER.generic.v1",
+						  "folders": [
+							{
+							  "_type": "FOLDER",
+							  "name": {
+								"_type": "DV_TEXT",
+								"value": "summary_compo_z"
+							  },
+							  "archetype_node_id": "openEHR-EHR-FOLDER.generic.v1"
+							}
+						  ]
+						},
+						{
+						  "_type": "FOLDER",
+						  "name": {
+							"_type": "DV_TEXT",
+							"value": "foldername-w-special-chars"
+						  },
+						  "archetype_node_id": "openEHR-EHR-FOLDER.generic.v1"
+						}
+					  ]
+					}
+				`),
+			},
+			false,
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
