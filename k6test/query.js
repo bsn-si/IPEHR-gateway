212364cbe6c1e64524769ca711578e521b616e24
Valeriy Tetevin <seal.matob@gmail.com>
2023-07-04 16:29:29 +0200

    Add methods for query

--- k6test/query.js
+++ k6test/query.js
@@ -4,14 +4,52 @@ import { randomString } from 'https://jslib.k6.io/k6-utils/1.4.0/index.js';
 import chai, { describe, expect } from 'https://jslib.k6.io/k6chaijs/4.3.4.3/index.js';
 
 
-// r.GET("/query/:qualified_query_name", a.Query.ExecStoredQuery)
-// r.POST("/query/:qualified_query_name", a.Query.PostExecStoredQuery)
-// r.GET("/query/aql", a.Query.ExecGetQuery)
 // r.POST("/query/aql", a.Query.ExecPostQuery)
+export function post_exec_query(ctx, aqlQueryString) {
+    const payload = JSON.stringify({});
+    const response = ctx.session.post(`/query/aql`, aqlQueryString, payload);
 
+    expect(response.status, "exec query").to.equal(200);
+    expect(response).to.have.validJsonBody();
 
-// query.GET("/definition/query/:qualified_query_name", a.Query.ListStored)
+    const resp = JSON.parse(response.body);
 
-// query.PUT("/definition/query/:qualified_query_name", a.Query.Store)
-// query.PUT("/definition/query/:qualified_query_name/:version", a.Query.StoreVersion)
-// query.GET("/definition/query/:qualified_query_name/:version", a.Query.GetStoredByVersion)
+    return resp;
+}
+
+// r.POST("/query/:qualified_query_name", a.Query.PostExecStoredQuery)
+export function post_exec_stored_query(ctx, qualified_query_name) {
+    const payload = JSON.stringify({});
+    const response = ctx.session.post(`/query/${qualified_query_name}`, payload);
+
+    expect(response.status, "exec stored query").to.equal(200);
+    expect(response).to.have.validJsonBody();
+
+    const resp = JSON.parse(response.body);
+
+    return resp;
+}
+
+// r.GET("/query/aql", a.Query.ExecGetQuery)
+export function get_exec_query(ctx, aqlQueryString) {
+    const response = ctx.session.get(`/query/aql/?$q=${aqlQueryString}`);
+
+    expect(response.status, "exec query").to.equal(200);
+    expect(response).to.have.validJsonBody();
+
+    const resp = JSON.parse(response.body);
+
+    return resp;
+}
+
+// r.GET("/query/:qualified_query_name", a.Query.ExecStoredQuery)
+export function get_exec_stored_query(ctx, qualified_query_name) {
+    const response = ctx.session.get(`/query/${qualified_query_name}`);
+
+    expect(response.status, "exec stored query").to.equal(200);
+    expect(response).to.have.validJsonBody();
+
+    const resp = JSON.parse(response.body);
+
+    return resp;
+}
